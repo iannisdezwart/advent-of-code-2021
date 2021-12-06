0fb47809dad8fdfbbdd95fc978b8b8880c7990f2
iannisdezwart <iannis__@hotmail.com>
2021-12-06 23:52:32 +0100

    🚀 Added C++ Solutions For Days 5 & 6

---
+++ C++/5B.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+
+#define N_LINES 500
+#define MAX_X 1000
+#define MAX_Y 1000
+
+struct Point
+{
+	int x;
+	int y;
+};
+
+struct Line
+{
+	Point p1;
+	Point p2;
+};
+
+void
+solve(std::vector<Line> &lines)
+{
+	int grid[MAX_Y][MAX_X] = { 0 };
+
+	for (auto [ p1, p2 ] : lines)
+	{
+		if (p1.x != p2.x && p1.y != p2.y)
+		{
+			int dx = p2.x - p1.x > 0 ? 1 : -1;
+			int dy = p2.y - p1.y > 0 ? 1 : -1;
+
+			for (int x = p1.x, y = p1.y;
+				x != p2.x + dx || y != p2.y + dy;
+				x += dx, y += dy)
+			{
+				grid[y][x]++;
+			}
+		}
+		else if (p1.x == p2.x)
+		{
+			int y_start = std::min(p1.y, p2.y);
+			int y_end = std::max(p1.y, p2.y);
+
+			for (int y = y_start; y <= y_end; y++)
+			{
+				grid[y][p1.x]++;
+			}
+		}
+		else
+		{
+			int x_start = std::min(p1.x, p2.x);
+			int x_end = std::max(p1.x, p2.x);
+
+			for (int x = x_start; x <= x_end; x++)
+			{
+				grid[p1.y][x]++;
+			}
+		}
+	}
+
+	int count = 0;
+
+	for (int y = 0; y < MAX_Y; y++)
+	{
+		for (int x = 0; x < MAX_X; x++)
+		{
+			if (grid[y][x] > 1)
+			{
+				count++;
+			}
+		}
+	}
+
+	std::cout << count << std::endl;
+}
+
+int
+main()
+{
+	std::vector<Line> lines(N_LINES);
+
+	for (int i = 0; i < N_LINES; i++)
+	{
+		scanf("%d,%d -> %d,%d\n", &lines[i].p1.x, &lines[i].p1.y, &lines[i].p2.x, &lines[i].p2.y);
+	}
+
+	solve(lines);
+}
---
+++ C++/6B.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+#define N_DAYS 256
+
+struct Population
+{
+	long long arr[9];
+
+	Population()
+	{
+		memset(arr, 0, sizeof(arr));
+	}
+
+	void
+	add(int n)
+	{
+		arr[n]++;
+	}
+
+	void
+	tick()
+	{
+		long long new_fish = arr[0];
+
+		for (int i = 0; i < 8; i++)
+		{
+			arr[i] = arr[i + 1];
+		}
+
+		arr[6] += new_fish;
+		arr[8] = new_fish;
+	}
+
+	size_t
+	size(){
+		size_t s = 0;
+
+		for (int i = 0; i < 9; i++)
+		{
+			s += arr[i];
+		}
+
+		return s;
+	}
+};
+
+void
+solve(Population &pop)
+{
+	for (int i = 0; i < N_DAYS; i++)
+	{
+		pop.tick();
+	}
+
+	std::cout << pop.size() << std::endl;
+}
+
+int
+main()
+{
+	Population pop;
+	int c = '\0';
+
+	while (c != EOF)
+	{
+		c = fgetc(stdin);
+		pop.add(c - '0');
+		c = fgetc(stdin);
+	}
+
+	solve(pop);
+}
